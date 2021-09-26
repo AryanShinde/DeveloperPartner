8a65c95f917fd9204cb56afc87179e267b05fcd6
AryanShinde <aryan.shinde20@vit.edu>
2021-09-26 20:57:30 +0530

    mogoDB connected

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+        console.log("mongoDB connected");
+    }
+    catch(err){
+        console.log(`there was an error: ${err.message}`);
+    }
+}
+module.exports=connectDB;
